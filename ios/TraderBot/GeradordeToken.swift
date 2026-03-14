f4e494d2e261426876615137b0fcc1290c7a2f7b
Teo986L <teodoro12lucamba@gmail.com>
2026-03-14 07:33:01 +0100

    Primeira versão - Gerador de Tokens

---
+++ ios/TraderBot/GeradordeToken.swift
@@ -0,0 +1,18 @@
+//
+//  GeradordeTokenApp.swift
+//  GeradordeToken
+//
+//  Aplicativo WebView para iOS 15+
+//
+
+import SwiftUI
+
+@main
+struct TraderBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
